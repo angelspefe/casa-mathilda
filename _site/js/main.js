8886da4f664b02f5ec4c1b3a64b676e5b222353f
Angels <angelspefe@hotmail.com>
2018-08-08 17:26:50 +0200

    changes

--- _site/js/main.js
+++ _site/js/main.js
@@ -62,6 +62,26 @@ $(document).ready(function() {
 });
 
 
+// - - - - - - - - - - - MAGNIFIC POP UP GALLERY
+
+$(document).ready(function() {
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+      arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+		}
+	});
+});
+
 
 
 // Smooth Scroll
